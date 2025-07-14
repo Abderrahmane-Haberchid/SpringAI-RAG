85dd10d74105bfd29cf34ce441a7213ddcf4f29a
abderahman <jhonsnowstark2@gmail.com>
2025-07-14 23:06:40 +0100

    adding react front and connecting to back + api calls done

--- rag-front/src/App.tsx
+++ rag-front/src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import axios from 'axios'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ask, setAsk] = useState('');
+  const [response, setResponse] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleAsk = async () => {
+    setLoading(true);
+      await axios.post('http://localhost:8080/api/chat', ask,
+        { 
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(response => {
+          console.log(response.data);
+          setResponse(response.data);
+          setLoading(false);
+        }).catch(error => {
+          console.error('Error:', error);
+        });
+  }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='wrapper'>
+      <div className='header'>
+        <h1>RAG Frontend</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='content'>
+        <h1>Ask AI</h1>
+
+        <div className='ask-form'>
+          <input type="text" onChange={(e) => setAsk(e.target.value)} />
+          <button disabled={loading} 
+                  onClick={handleAsk}
+                  >
+                    {loading ? 'Loading...' : 'Ask'}
+          </button>
+        </div>
+
+        <div className='response'>
+          <h2>Response</h2>
+          <p>{response}</p>
+        </div>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
--- rag-springAI-back/src/main/java/net/abdo/rag_springAI/controller/ChatController.java
+++ rag-springAI-back/src/main/java/net/abdo/rag_springAI/controller/ChatController.java
@@ -4,13 +4,11 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chat")
+@CrossOrigin("*")
 public class ChatController {
 
     private final OllamaChatModel ollamaChatModel;
@@ -23,12 +21,16 @@ public class ChatController {
 
     @PostMapping
     public String chat(@RequestBody String prompt){
-        return ChatClient.builder(ollamaChatModel)
-                .build()
-                .prompt()
-                .advisors(new QuestionAnswerAdvisor(vectorStore))
-                .user(prompt)
-                .call()
-                .content();
+         String rawResponse = ChatClient.builder(ollamaChatModel)
+                                    .build()
+                                    .prompt()
+                                    .advisors(new QuestionAnswerAdvisor(vectorStore))
+                                    .user(prompt)
+                                    .call()
+                                    .content();
+        if (rawResponse == null)
+            throw new IllegalStateException("No data available");
+
+        return rawResponse.replaceAll("(?s)<think>.*?</think>", "").trim();
     }
 }
